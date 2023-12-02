555a9aefe743d0902d022c9c2f61f32b9dd89cb6
Nick777-Pixel <81410494+Nick777-Pixel@users.noreply.github.com>
2023-12-02 17:36:53 -0600

    Update ai-configs.ts

--- src/configs/ai-configs.ts
+++ src/configs/ai-configs.ts
@@ -15,7 +15,7 @@ Link: https://platform.openai.com/docs/models/overview
 // Model Parameters
 // ==========================
 
-export const MODEL_NAME: string = "gpt-4-32k"
+export const MODEL_NAME: string = "gpt-4"
 
 // Higher Temp = More Creative
 // Set a low temp to keep responses inline with the source docs
